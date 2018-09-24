d41e551653306837b41105735591da74ddd2df2a
Hassan dad khan <hassandadkhanit@gmail.com>
2018-09-24 08:17:49 -0700

    Folders Added

---
+++ IOS_Architecture/IOS_Architecture/Shared/AppStateManager.swift
@@ -0,0 +1,13 @@
+//
+//  AppStateManager.swift
+//  IOS_Architecture
+//
+//  Created by Hassan dad khan on 24/09/2018.
+//  Copyright © 2018 Hassan dad khan. All rights reserved.
+//
+
+import Foundation
+
+class AppStateManager {
+    
+}
